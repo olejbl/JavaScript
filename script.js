90bddc213fee2575045598c4d8c870b1e6311143
Ole Jacob Brunstad <ole_jacob2@hotmail.com>
2018-08-01 02:56:00 +0200

    Truthy and Falsy values and equality operators

--- script.js
+++ script.js
@@ -235,6 +235,8 @@ console.log(drink);
 
 */
 
+/*
+
 // Switch statement
 var job = 'instructor';
 switch (job) {
@@ -287,10 +289,27 @@ if (age <13) {
 */
 
 
+/*******************
+* Truthy and Falsy values and equality operators
+*/
+
+// falsy values: undefined, null, 0, '', NaN
+// truthy values: NOT falsy values
+
+var height;
+
+height = 23;
+if (height || height === 0) {
+    console.log('Variable is defined');
+}   else {
+    console.log('Variable has NOT been defined')
+}
 
 
-
-
+// Equality operators
+if (height == '23') {
+    console.log('The == operator does type coercion!');
+} 
 
 
 
